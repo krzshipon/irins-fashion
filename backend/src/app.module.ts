3d43250bcf6ca712715f435bc1747ca570e7ff8d
krz.shipon1 <krz.shipon1@gmail.com>
2026-01-03 22:40:32 +0600

    feat: integrate Prisma ORM and add global HTTP request logging middleware

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,22 @@
-import { Module } from '@nestjs/common';
+import { Module, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { ActionLoggerMiddleware } from './common/middleware/logger.middleware';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    PrismaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(ActionLoggerMiddleware)
+      .forRoutes({ path: '*', method: RequestMethod.ALL });
+  }
+}
